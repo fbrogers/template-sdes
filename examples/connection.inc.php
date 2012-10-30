4086d69278898b0bba9bf556ce482e2fd6a96c9f
Jordan Rogers <fbrogers@gmail.com>
2012-10-30 18:16:55 -0400

    fixed db example

--- examples/connection.inc.php
+++ examples/connection.inc.php
@@ -3,7 +3,11 @@
 	require_once('C:\WebDFS\Websites\_phplib\sdestemplate\template_db.php');
 
 	//database connection
-	$instance = db::instance();
-	$instance->connection_string($server, $database, $username, $password);
-	$conn = $instance::connect();
+	try{
+		$instance = db::instance();
+		$instance->connection_string($server, $database, $username, $password);
+		$conn = $instance::connect();
+	} catch(Exception $e){
+		die('Error: '.$e->getMessage());
+	}
 ?>
