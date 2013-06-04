ed3e38587511690fa6d0644b7edefa3e0d991916
Jordan Rogers <fbrogers@gmail.com>
2013-06-04 13:00:10 -0400

    removed old css, updated examples

--- examples/connection.inc.php
+++ examples/connection.inc.php
@@ -1,13 +1,10 @@
 <?php
-	//require class
-	require_once('C:\WebDFS\Websites\_phplib\sdestemplate\template_db.php');
-
-	//database connection
-	try{
-		$instance = db::instance();
-		$instance->connection_string($server, $database, $username, $password);
-		$conn = $instance::connect();
-	} catch(Exception $e){
-		die('Error: '.$e->getMessage());
-	}
+  require_once('C:\WebDFS\Websites\_phplib\sdestemplate\template_db.php');
+  try{
+    $instance = db::instance();
+    $instance->connection_string($server, $database, $username, $password);
+    $conn = $instance::connect();
+  } catch(Exception $e){
+    die('Error: '.$e->getMessage());
+  }
 ?>
